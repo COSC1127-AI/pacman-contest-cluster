b47747731785aef06473a23220e2fcd568fb17b8
Sebastian Sardina <ssardina@gmail.com>
2019-08-10 14:04:52 +1000

    Changed title of simulator

--- contest/captureGraphicsDisplay.py
+++ contest/captureGraphicsDisplay.py
@@ -286,7 +286,7 @@ class PacmanGraphics:
     begin_graphics(screen_width,
                    screen_height,
                    BACKGROUND_COLOR,
-                   "CS188 Pacman")
+                   "UC Pacman Simulator")
 
   def drawPacman(self, pacman, index):
     position = self.getPosition(pacman)
