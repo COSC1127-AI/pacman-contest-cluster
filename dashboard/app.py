11017650f4aea132fcbd41eaffeb2fe8dd15899d
Sebastian <ssardina@gmail.com>
2021-06-11 10:01:06 +0000

    Updated dashboard pointers to www

--- dashboard/app.py
+++ dashboard/app.py
@@ -20,7 +20,7 @@ import streamlit.components.v1 as components
 from pandas import json_normalize 
 
 
-DATA_URL = ('/mnt/ssardina-pacman/cosc1125-1127-AI/www/feedback-final/stats-archive')
+DATA_URL = ('/mnt/ssardina-pacman/cosc1125-1127-AI20/project-contest/www/feedback-final/stats-archive')
 DEPLOYED_URL = 'http://118.138.246.177/feedback-final'
 ORGANIZER = 'RMIT AI - COSC1125/1127'
 
